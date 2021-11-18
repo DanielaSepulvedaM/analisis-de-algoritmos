6591c6674d3e2db4d0ce3e64e8c5f89c93046437
ingri daniela sepulveda montaña <isepulvedam@ucentral.edu.co>
2021-11-17 22:21:53 -0500

    adiciono el algoritmo bubbleSort

---
+++ OrdenamientoMercancia/Algoritmos/BubbleSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace OrdenamientoMercancia.Algoritmos
+{
+    public class BubbleSort : IOrdenamiento
+    {
+        private Stopwatch stopWatch = new Stopwatch();
+        public double TiempoTranscurrido => stopWatch.Elapsed.TotalSeconds;
+
+        public T[] Ordenar<T>(IEnumerable<T> datos, Func<T, T, bool> comparador, Func<bool> procesoCancelado)
+        {
+            T t;
+            var a = datos.ToArray();
+
+            stopWatch.Restart();
+            for (int p = 0; p <= a.Length - 2; p++)
+            {
+                if (procesoCancelado())//cancelar
+                    break;
+                for (int i = 0; i <= a.Length - 2; i++)
+                {
+                    if (comparador(a[i] , a[i + 1]))
+                    {
+                        t = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = t;
+                    }
+                }
+            }
+            stopWatch.Stop();
+            return a;
+        }
+    }
+}
