cbd1eadab6808271efba39e03e472c78d27243ea
ingri daniela sepulveda montaña <isepulvedam@ucentral.edu.co>
2021-11-17 22:22:52 -0500

    adiciono segundo Grid y la funcion para agregar las repeticiones

--- OrdenamientoMercancia/Form1.cs
+++ OrdenamientoMercancia/Form1.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -34,13 +35,18 @@ namespace OrdenamientoMercancia
             //crea un hilo en segundo plano para correr el segundo plano 
             TareaSegundoPlano = new BackgroundWorker();
             TareaSegundoPlano.WorkerSupportsCancellation = true;
+            TareaSegundoPlano.WorkerReportsProgress = true;
             TareaSegundoPlano.DoWork += new DoWorkEventHandler(OrdenarEnvios);
             TareaSegundoPlano.RunWorkerCompleted += new RunWorkerCompletedEventHandler(OrdenamientoTerminado);
+            TareaSegundoPlano.ProgressChanged += TareaSegundoPlano_ProgressChanged;
         }
 
         private void grid_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
             DataGridViewColumn newColumn = grid.Columns[e.ColumnIndex];
+
+            if (newColumn.SortMode == DataGridViewColumnSortMode.NotSortable)
+                return;
 
             foreach (var item in grid.Columns.Cast<DataGridViewColumn>())
                 item.HeaderCell.SortGlyphDirection = SortOrder.None;
@@ -70,7 +76,13 @@ namespace OrdenamientoMercancia
             lblOrdenando.Visible = true;
             Cursor.Current = Cursors.WaitCursor;
 
-            TareaSegundoPlano.RunWorkerAsync(new Argumentos { direction = direction, columna = newColumn.DataPropertyName, algoritmo = Algoritmos.SelectedIndex, cantidad = int.Parse(txtCantidad.Text) });
+            TareaSegundoPlano.RunWorkerAsync(new Argumentos { 
+                direction = direction, 
+                columna = newColumn.DataPropertyName, 
+                algoritmo = Algoritmos.SelectedIndex, 
+                cantidad = int.Parse(txtCantidad.Text),
+                repeticiones = int.Parse(txtRepeticiones.Text)
+            });
         }
 
 
@@ -79,46 +91,89 @@ namespace OrdenamientoMercancia
             Argumentos args = (Argumentos)e.Argument;
             var w = (BackgroundWorker)sender;
 
-            IOrdenamiento ordernador = args.algoritmo == 0 ? (IOrdenamiento)new InsertionSort() : (IOrdenamiento)new SelectionSort();
+            IOrdenamiento ordernador = (IOrdenamiento)new InsertionSort();
+            switch (args.algoritmo)
+            {
+                case 0:
+                    ordernador = (IOrdenamiento)new InsertionSort();
+                    break;
+                case 1:
+                    ordernador = (IOrdenamiento)new SelectionSort();
+                    break;
+                case 2:
+                    ordernador = (IOrdenamiento)new BubbleSort();
+                    break;
+            }
+            
 
             using var context = new Bd();
-
-            var datos = context.Shipments.Take(args.cantidad).ToList();//carga de la BD la cant de registros deseados
-
             Shipment[] datosOrdenados = null;
-
-            switch (args.columna)
+            for (int i = 0; i < args.repeticiones && !w.CancellationPending; i++)
             {
-                case "OriginalWeight":
-                    if (direction == ListSortDirection.Descending)
-                        datosOrdenados = ordernador.Ordenar(datos, (a, b) => a.OriginalWeight > b.OriginalWeight, () => w.CancellationPending);
-                    else
-                        datosOrdenados = ordernador.Ordenar(datos, (a, b) => a.OriginalWeight < b.OriginalWeight, () => w.CancellationPending);
-                    break;
-                case "ShipmentDate":
-                    if (direction == ListSortDirection.Descending)
-                        datosOrdenados = ordernador.Ordenar(datos, (a, b) => a.ShipmentDate > b.ShipmentDate, () => w.CancellationPending);
-                    else
-                        datosOrdenados = ordernador.Ordenar(datos, (a, b) => a.ShipmentDate < b.ShipmentDate, () => w.CancellationPending);
-                    break;
-                case "SndrCity":
-                    if (direction == ListSortDirection.Descending)
-                        datosOrdenados = ordernador.Ordenar(datos, (a, b) => string.Compare(a.SndrCity, b.SndrCity) > 0, () => w.CancellationPending);
-                    else
-                        datosOrdenados = ordernador.Ordenar(datos, (a, b) => -string.Compare(a.SndrCity, b.SndrCity) > 0, () => w.CancellationPending);
-                    break;
-                case "RecCity":
-                    if (direction == ListSortDirection.Descending)
-                        datosOrdenados = ordernador.Ordenar(datos, (a, b) => string.Compare(a.RecCity, b.RecCity) > 0, () => w.CancellationPending);
-                    else
-                        datosOrdenados = ordernador.Ordenar(datos, (a, b) => -string.Compare(a.RecCity, b.RecCity) > 0, () => w.CancellationPending);
-                    break;
-                default:
-                    break;
+                var datos = context.Shipments.Take(args.cantidad).ToList();//carga de la BD la cant de registros deseados
+
+                switch (args.columna)
+                {
+                    case "OriginalWeight":
+                        if (direction == ListSortDirection.Descending)
+                            datosOrdenados = ordernador.Ordenar(datos, (a, b) => a.OriginalWeight > b.OriginalWeight, () => w.CancellationPending);
+                        else
+                            datosOrdenados = ordernador.Ordenar(datos, (a, b) => a.OriginalWeight < b.OriginalWeight, () => w.CancellationPending);
+                        break;
+                    case "ShipmentDate":
+                        if (direction == ListSortDirection.Descending)
+                            datosOrdenados = ordernador.Ordenar(datos, (a, b) => a.ShipmentDate > b.ShipmentDate, () => w.CancellationPending);
+                        else
+                            datosOrdenados = ordernador.Ordenar(datos, (a, b) => a.ShipmentDate < b.ShipmentDate, () => w.CancellationPending);
+                        break;
+                    case "SndrCity":
+                        if (direction == ListSortDirection.Descending)
+                            datosOrdenados = ordernador.Ordenar(datos, (a, b) => string.Compare(a.SndrCity, b.SndrCity) > 0, () => w.CancellationPending);
+                        else
+                            datosOrdenados = ordernador.Ordenar(datos, (a, b) => -string.Compare(a.SndrCity, b.SndrCity) > 0, () => w.CancellationPending);
+                        break;
+                    case "RecCity":
+                        if (direction == ListSortDirection.Descending)
+                            datosOrdenados = ordernador.Ordenar(datos, (a, b) => string.Compare(a.RecCity, b.RecCity) > 0, () => w.CancellationPending);
+                        else
+                            datosOrdenados = ordernador.Ordenar(datos, (a, b) => -string.Compare(a.RecCity, b.RecCity) > 0, () => w.CancellationPending);
+                        break;
+                    default:
+                        break;
+                }
+
+                TareaSegundoPlano.ReportProgress(0, new Iteracion
+                {
+                    algoritmo = args.algoritmo,
+                    cantidad = args.cantidad,
+                    tiempoTranscurrido = ordernador.TiempoTranscurrido,
+                    iteracionNumero = i + 1
+
+                }) ;
             }
 
             e.Result = new Resultado { Envios = datosOrdenados, TiempoTranscurrido = ordernador.TiempoTranscurrido };
            
+        }
+
+        private void TareaSegundoPlano_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            var it = (Iteracion)e.UserState;
+            string nombreAlgoritmo = string.Empty;
+            switch (it.algoritmo)
+            {
+                case 0:
+                    nombreAlgoritmo = "InsertionSort";
+                    break;
+                case 1:
+                    nombreAlgoritmo = "SelectionSort";
+                    break;
+                case 2:
+                    nombreAlgoritmo = "BubbleSort";
+                    break;
+
+            }
+            gridInforme.Rows.Add(it.iteracionNumero, it.tiempoTranscurrido, nombreAlgoritmo);
         }
 
         private void OrdenamientoTerminado(object sender, RunWorkerCompletedEventArgs e)
@@ -154,6 +209,11 @@ namespace OrdenamientoMercancia
         {
 
         }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 
     public class Argumentos {
@@ -163,6 +223,20 @@ namespace OrdenamientoMercancia
 
         public int algoritmo { get; set; }
         public int cantidad { get; set; }
+        public int repeticiones { get; set; }
+    }
+
+    public class Iteracion
+    {
+        public double tiempoTranscurrido { get; set; }
+        public int algoritmo { get; set; }
+        public int cantidad { get; set; }
+        public int iteracionNumero { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return $"iteracion: {iteracionNumero}, tiempo: {tiempoTranscurrido}";
+        //}
     }
 
     public class Resultado {
